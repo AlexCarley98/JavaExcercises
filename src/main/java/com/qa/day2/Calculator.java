b6760c295422409432875515e8fbb03ea4296339
ACarley <Alex.carley98@gmail.com>
2020-08-10 15:36:28 +0100

    Added package

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -1,8 +1,10 @@
+package com.qa.day2;
+
 public class Calculator{
     public static void main(String[]args){
-        int num1 = 6
-        int num2 = 7
-        int added = num1 + num2
-        System.out.println(added)
+        int num1 = 6;
+        int num2 = 7;
+        int added = num1 + num2;
+        System.out.println(added);
     }
 }
