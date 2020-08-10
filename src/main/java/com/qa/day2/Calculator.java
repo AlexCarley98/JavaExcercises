00b80a7dfe0ed7da769a9f1055642510a1bc7260
ACarley <Alex.carley98@gmail.com>
2020-08-10 15:52:07 +0100

    Added subtraction, division and multiplication.

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -5,7 +5,19 @@ public class Calculator{
         int num1 = 6;
         int num2 = 7;
         System.out.println(addition(num1, num2));
+        System.out.println(multiplication(num1, num2));
+        System.out.println(subtraction(num1, num2));
+        System.out.println(division(num1, num2));
 
+    }
+    public static int multiplication(int num1, int num2){
+        return num1*num2;
+    }
+    public static int subtraction(int num1, int num2){
+        return num1-num2;
+    }
+    public static int division(int num1, int num2){
+        return num1/num2;
     }
     public static int addition(int num1, int num2){
         return num1 + num2;
