d63d45a954cc407532f84590324704ef0cafd412
ACarley <Alex.carley98@gmail.com>
2020-08-10 15:32:16 +0100

    Added calculator class

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -1,6 +1,8 @@
-public static void main(String[]args){
+public class Calculator{
+    public static void main(String[]args){
         int num1 = 6
         int num2 = 7
         int added = num1 + num2
         System.out.println(added)
- x       }
+    }
+}
