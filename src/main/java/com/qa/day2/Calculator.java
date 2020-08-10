d4a5396753ab622c98702a9d61087704a7c9fbc0
ACarley <Alex.carley98@gmail.com>
2020-08-10 16:10:53 +0100

    Now will only use the division method if num 1 is lower than num2.

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -2,14 +2,19 @@ package com.qa.day2;
 
 public class Calculator{
     public static void main(String[]args){
-        int num1 = 6;
+        int num1 = 96;
         int num2 = 7;
         System.out.println(addition(num1, num2));
         System.out.println(multiplication(num1, num2));
         System.out.println(subtraction(num1, num2));
-        System.out.println(division(num1, num2));
+        if(num1 < num2) {
+            System.out.println(division(num1, num2));
+            }
+        else {
+            System.out.println("Division can't be performed");
+            }
+        }
 
-    }
     public static int multiplication(int num1, int num2){
         return num1*num2;
     }
@@ -17,7 +22,7 @@ public class Calculator{
         return num1-num2;
     }
     public static double division(double num1, double num2){
-        return num1/num2;
+            return num1/num2;
     }
     public static int addition(int num1, int num2){
         return num1 + num2;
