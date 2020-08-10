d1920eee5da1f13d02011134246cc788bcbe25c2
ACarley <Alex.carley98@gmail.com>
2020-08-10 15:49:17 +0100

    Immproved structure, addition is now a method.

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -4,7 +4,10 @@ public class Calculator{
     public static void main(String[]args){
         int num1 = 6;
         int num2 = 7;
-        int added = num1 + num2;
-        System.out.println(added);
+        System.out.println(addition(num1, num2));
+
+    }
+    public static int addition(int num1, int num2){
+        return num1 + num2;
     }
 }
