f8155fc6e7fda6a982b017cef597f5b4d14b0c37
ACarley <Alex.carley98@gmail.com>
2020-08-10 16:35:50 +0100

    Checks for pass or fail on overall mark

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -4,7 +4,7 @@ public class Results{
 
     public static void main(String[] args) {
    int total = marks(87,60,110);
-   System.out.println(percentage(total));
+   percentage(total);
     }
     public static int marks(int Biol, int Phys, int Chem){
         System.out.println(Biol);
@@ -14,7 +14,13 @@ public class Results{
         System.out.println(total);
         return total;
     }
-    public static double percentage(double total){
-    return (total*100)/450;
+    public static void percentage(double total){
+    double totalP = (total*100)/450;
+    if(totalP >= 60){
+        System.out.println("Overall grade of "+totalP+" (Pass)");
+    }
+    else{
+        System.out.println("Overall grade of "+totalP+" (Fail)");
+    }
     }
 }
