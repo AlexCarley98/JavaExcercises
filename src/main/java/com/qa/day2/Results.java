b966f17eee9c8a6fb24fa5968ebef2a7c0baff53
ACarley <Alex.carley98@gmail.com>
2020-08-11 10:05:11 +0100

    Fails if a single module is under 60% and tells you how many moduels you failed.

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -3,8 +3,24 @@ package com.qa.day2;
 public class Results{
 
     public static void main(String[] args) {
-   int total = marks(87,60,110);
-   percentage(total);
+        int Biol = 147;
+        int Phys = 15;
+        int Chem = 11;
+        int Fails = 0;
+        int total = marks(Biol,Phys,Chem);
+        boolean BiolP = BiolPercentage(Biol);
+        if (!BiolP){
+            Fails++;
+        }
+        boolean PhysP = PhysPercentage(Phys);
+        if (!PhysP){
+            Fails++;
+        }
+        boolean ChemP = ChemPercentage(Chem);
+        if (!ChemP){
+            Fails++;
+        }
+        percentage(total,BiolP,PhysP,ChemP,Fails);
     }
     public static int marks(int Biol, int Phys, int Chem){
         System.out.println(Biol);
@@ -14,13 +30,36 @@ public class Results{
         System.out.println(total);
         return total;
     }
-    public static void percentage(double total){
+    public static boolean BiolPercentage(double Biol){
+        if(((Biol*100)/150)>=60){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean PhysPercentage(double Phys) {
+        if (((Phys * 100) / 150) >= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean ChemPercentage(double Chem) {
+        if (((Chem * 100) / 150) >= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void percentage(double total,boolean BiolP, boolean PhysP, boolean ChemP, int Fails){
     double totalP = (total*100)/450;
-    if(totalP >= 60){
+    if(BiolP && PhysP && ChemP){
         System.out.println("Overall grade of "+totalP+" (Pass)");
     }
     else{
-        System.out.println("Overall grade of "+totalP+" (Fail)");
+        System.out.println("Overall grade of "+totalP+" (Failed: "+Fails+")");
     }
     }
 }
