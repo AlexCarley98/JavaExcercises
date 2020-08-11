09be9be8dad1edbe72fe0c413b787a14203c2cab
ACarley <Alex.carley98@gmail.com>
2020-08-11 11:57:13 +0100

    Prints letters based on value

--- src/main/java/com/qa/day3/flowchart.java
+++ src/main/java/com/qa/day3/flowchart.java
@@ -1,15 +1,45 @@
+package com.qa.day3;
+
 public class flowchart {
 
     public static void main(String[] args) {
-    System.out.println(input(2,3,false));
+    input(431);
     }
-    public static int input(int num1, int num2, boolean TF){
-        if(TF){
-            return num1+num2;
+    public static void input(int A){
+        if(A > 2000){
+            System.out.println("A");
+            if(A > 6000){
+                System.out.println("C");
+            }
+            else{
+                if(A>4000){
+                    System.out.println("D");
+                }
+                else{
+                    System.out.println("E");
+                }
+            }
         }
-        else {
-            return num1*num2;
-
+        else{
+            System.out.println("1");
+            if(A>100){
+                System.out.println("3");
+                if(A>600){
+                    System.out.println("5");
+                }
+                else{
+                    System.out.println("4");
+                    if (A>500){
+                        System.out.println("6");
+                    }
+                    else{
+                        System.out.println("7");
+                    }
+                }
+            }
+            else{
+                System.out.println("2");
+            }
         }
     }
 }
